b48c99537f4a7b5f84b98878268a16d9716cc7c8
pratyushranjan1729 <119096130+pratyushranjan1729@users.noreply.github.com>
2022-12-01 00:00:37 +0530

    Delete class24-Split_Function-CipherSchool.py

--- class24-Split_Function-CipherSchool.py
+++
@@ -1,8 +0,0 @@
-# name=input("enter your name")
-# age=input("enter your age")
-# name,age=input("enter your name and age ").split()
-# print("your name is",name)
-# print("your age is",age)
-name , age = input("enter your name and age ").split(",")
-print(name)
-print(age)
